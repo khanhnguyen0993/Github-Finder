76b4f0310daa56fc781f7a82ecf75e5bf2b076b4
Khanh <khanhnguyen0993@gmail.com>
2019-09-15 00:40:25 +1000

    React Router Setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
+import Alert from './components/layout/Alert';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component{
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   }
 
   // Life Cycle Method
@@ -17,25 +21,55 @@ class App extends Component{
 
     // Load data from api when the app loads.
     const res = await axios
-      .get('https://api.github.com/users');
+      .get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secrect=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({users: res.data, loading: false});
   }
 
+  // Search User: text comes from Search.js with props
+  searchUsers = async text => {
+    this.setState({loading: true});
+
+    const res = await axios
+      .get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secrect=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({users: res.data.items, loading: false});
+  }
+
+  // Clear users from state
+  clearUsers = () => this.setState({users: [], loading: false});
+
+  // Set alert
+  setAlert = (msg, type) => {
+    //this.setState({alert: {msg: msg, type: type}});
+    this.setState({alert : {msg,type}});
+    setTimeout(()=> this.setState({alert:null}), 3000);
+  };
+
+
   render() {
+    // destruct users & loading from this.state
+    // wherever has this.state.users -> just type users
+    const {users, loading, alert} = this.state;
 
     return (
-      <div className="App">
-        <Navbar title=" Github Finder" icon="fa fa-github"/>
-        <div className='container'>
-          <Users loading={this.state.loading} users={this.state.users}/>
+      <Router>
+        <div className="App">
+          <Navbar title=" Github Finder" icon="fa fa-github"/>
+          <div className='container'>
+            <Alert alert={alert} />
+            <Search 
+              searchUsers={this.searchUsers} 
+              clearUsers={this.clearUsers} 
+              showClear={users.length > 0 ? true:false}
+              setAlert={this.setAlert}/>
+            <Users 
+              loading={loading} 
+              users={users} />
+          </div>
         </div>
-      </div>
+      </Router>
     
-      /// return Reach.createElement('div, {className: 'App'},
-      // React.createElement('h1', null, 'Hello from React'))
-      // showName ? name : null == showName && name   : only show the name if showName is true
-      // {loading ? <h4>Loading ... </h4> : <h1>Hello {showName && name.toUpperCase()}</h1>}
     );
   }
 }
